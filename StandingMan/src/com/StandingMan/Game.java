df0d9cd2fe9448a503541234d648c740b364c77e
estrick <elliot-strickland@hotmail.com>
2017-09-12 20:52:07 +1000

    Created StandingMan, save errors/corrupt file with WalkingMan

---
+++ StandingMan/src/com/StandingMan/Game.java
@@ -0,0 +1,14 @@
+package com.WalkingMan;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable {
+
+    private static final long serialVersionUID = 1L;
+    public static final int WIDTH = 160;
+    public static final int HEIGHT = (WIDTH / (12*9));
+
+    public void run() {}
+
+
+}
--- WalkingMan/src/com/WalkingMan/Main.java
+++
@@ -1,8 +0,0 @@
-package com.WalkingMan;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
